e70ca074e6f0c33668e65d286df32c56b8784f50
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2025-01-24 19:34:52 +0100

    fix

--- app/company/c_profile/page.tsx
+++ app/company/c_profile/page.tsx
@@ -1,5 +1,7 @@
 import CompanyProfile from '@/components/elements/company/company_profile/CompanyProfile'
 
 export default function Page() {
-	return <CompanyProfile />
+	// return <CompanyProfile />
+
+	return true
 }
